dce57521583295f7fb8297781f60503364d02656
dionnephualiann <dphua@ringling.edu>
2017-05-23 14:33:34 +0800

    Js practice

--- independent-practice.js
+++ independent-practice.js
@@ -3,6 +3,16 @@
 // number is the length of the corresponding string.
 
 // your code here
+var array = ["cat", "doggie","rabbit"]
+var newArray = [];
+var lengths = function(array) {
+    for( var i= 0; i< array.length; i++) {
+    	newArray.push(array[i].length);
+    }
+  console.log(newArray);
+}  
+
+lengths(array);
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -13,8 +23,23 @@
 
 // your code here
 
+var transmogrifier = function(a,b,c) {
+	var sum = a+b
+	return Math.pow(sum,c);
+}
+
+transmogrifier(2,3,4);
+
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
 // your code here
+
+var wordReverse = function(x) {
+    var str = x
+	console.log(str.split("").reverse().join(""));
+}
+
+wordReverse("zazam");
+
